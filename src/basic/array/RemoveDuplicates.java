299784b9f227d864669e266acb95a1610baeb29f
amber <anlanting@sinotrans.com>
2021-05-11 15:07:48 +0800

    Initial commit

---
+++ src/basic/array/RemoveDuplicates.java
@@ -0,0 +1,21 @@
+package basic.array;
+
+/**
+ * @description:
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ *
+ * @author: amber
+ * @time: 2021-5-11 15:01
+ */
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int removeDuplicates(int[] nums) {
+        return 0;
+    }
+}
